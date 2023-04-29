fbc6d3644b3f2935cfd0664a811f81f33251a9e3
harrykamboj1 <singhharnoor116@gmail.com>
2023-04-29 17:20:38 +0530

    Second commit

---
+++ build/precache-manifest.7b992850442573f928cd35ad0fe297b3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c061db99e18e7e452303f5c90781cbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "199f40ad9b6c9c65e54b",
+    "url": "/static/js/2.4c9de9a1.chunk.js"
+  },
+  {
+    "revision": "1646ee02996b22a7d685",
+    "url": "/static/js/main.4080892c.chunk.js"
+  },
+  {
+    "revision": "84832877ed76605d723e",
+    "url": "/static/js/runtime-main.aa743321.js"
+  }
+]);
